0963f7d1329578e9508471088a4a3347a2e52c5f
ianshulkumar <100278973+ianshulkumar@users.noreply.github.com>
2022-03-06 17:47:59 +0530

    Update linearsearch.c

--- LAB-ASSIGNMENTS/linearsearch.c
+++ LAB-ASSIGNMENTS/linearsearch.c
@@ -1,3 +1,7 @@
+// Problem I. Given an array of nonnegative integers, design a linear algorithm and implement it using a
+// program to find whether given key element is present in the array or not. Also, find total number
+// of comparisons for each input case. (Time Complexity = O(n), where n is the size of input)
+
 #include<stdio.h>
 int linearsearch(int arr[],int n,int key){
   int i;
